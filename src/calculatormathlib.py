06a8c188e1137e48ec8f61719875e823038b09d4
Pavel Hajek <xhajekp00@vutbr.cz>
2024-04-09 16:34:33 +0200

    Added doxygen comments to mathlib

--- src/calculatormathlib.py
+++ src/calculatormathlib.py
@@ -1,11 +1,37 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+##
+# @file calculatormathlib.py
+#
+# @brief Math library for calculator
+#
+# @section description_calculatormathlib Description
+# - Math library with all functions used by the calculator
+#   (+, -, *, /, ^, 2√, n√, %)
+#
+# @section todo_calculatormathlib TODO
+# Add functions - sub, mul, div, pow, mod
+#
+# @section author_calculatormathlib Author(s)
+# xnovott00, xhajekp00
+
 class CalculatorMathLib:
+    """! Class with math functions.
+
+    Class with all the math functions used by calculator.
+    """
 
     #+
     @staticmethod
     def add(a, b):
+        """! Function to compute a sum of two numbers.
+
+        @param a First number to add.
+        @param b Second number to add.
+
+        @return Sum of the two numbers.
+        """
         return a + b
     
     #-
@@ -19,6 +45,13 @@ class CalculatorMathLib:
     #2√
     @staticmethod
     def sqrt(a):
+        """! Function to compute a square root of given number.
+
+        @param a Number to compute the square root of.
+
+        @return Square root of given number.
+        @return ValueError if given number is negative.
+        """
         if (a < 0):
             return ValueError('ValueError')
         else:
@@ -27,6 +60,14 @@ class CalculatorMathLib:
     #n√
     @staticmethod
     def root(a, n):
+        """! Function to compute a nth root of given number.
+
+        @param a Number to compute the Nth root of.
+        @param n Number representing Nth root.
+
+        @return Nth root of given number.
+        @return Returns ValueError if "n" is even and "a" is negative.
+        """
         if (n%2 == 0) and (a < 0):
             return ValueError('ValueError')
         else:
@@ -35,6 +76,13 @@ class CalculatorMathLib:
     #!
     @staticmethod
     def factorial(a):
+        """! Function to compute a factorial of given number.
+
+        @param a Number to compute the factorial of.
+
+        @return Returns factorial of given number.
+        @return ValueError if given number isn't integer or if it is negative.
+        """
         if type(a) != int or a < 0:
             return ValueError('ValueError')
         if a == 0 or a == 1:
