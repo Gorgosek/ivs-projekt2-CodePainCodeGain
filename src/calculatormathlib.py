7ad9ec7dc853e12288101d27ff9f03f7d1581822
Pavel Hajek <xhajekp00@vutbr.cz>
2024-04-09 16:13:00 +0200

    src/calculatormathlib.py - updated factorial function and added root functions

--- src/calculatormathlib.py
+++ src/calculatormathlib.py
@@ -7,6 +7,7 @@ class CalculatorMathLib:
     @staticmethod
     def add(a, b):
         return a + b
+    
     #-
 
     #*
@@ -15,14 +16,30 @@ class CalculatorMathLib:
 
     #^
 
-    #√
+    #2√
+    @staticmethod
+    def sqrt(a):
+        if (a < 0):
+            return ValueError('ValueError')
+        else:
+            return round(a**0.5, 10)
+        
+    #n√
+    @staticmethod
+    def root(a, n):
+        if (n%2 == 0) and (a < 0):
+            return ValueError('ValueError')
+        else:
+            return round(a**(1/n), 10)
 
     #!
+    @staticmethod
     def factorial(a):
-        if type(a) != int or n < 0:
+        if type(a) != int or a < 0:
             return ValueError('ValueError')
         if a == 0 or a == 1:
             return 1
         else:
             return a * CalculatorMathLib.factorial(a-1)
+        
     #%
