9b08a126f82b1e24a7a523752836e70269680fcc
GorgosekPC <nezbeda.adam@gmail.com>
2024-04-20 00:49:49 +0200

    Added Makefile for automatized testing and a numpy version of project

---
+++ src/np_standard_deviation.py
@@ -0,0 +1,53 @@
+import cProfile
+import pstats
+from pstats import SortKey
+import sys # for stdin use
+import numpy as np
+from calculatormathlib import CalculatorMathLib # Used for CalculatorMathLib() object
+
+
+def read_stdin():
+    input_data = sys.stdin.read()
+    float_vals = np.array(input_data.split(), dtype=np.float64)
+    return float_vals
+
+
+# Object for mathlib functions
+Math = CalculatorMathLib()
+
+# Sums the difference of (num - barNum)^2 for all numbers from the array nums
+# barNum is supposed to be the mean
+def sum_of_squared_differences(nums, barNum):
+    res = 0
+    for num in nums:
+        powRes = Math.pow2(num - barNum)
+        res = Math.add(res, powRes)
+    return res
+
+
+def standard_deviation(nums):
+    sumOfNums = np.sum(nums)
+    argsLen = len(nums)     # Accounts for 0eth element correctly
+
+    # barX median also medX
+    barX = Math.div(sumOfNums, argsLen)
+
+    # Final equation using s = sqrt(sum_of_squared_differences/argsLen-1)
+    S = Math.sqrt(Math.div(sum_of_squared_differences(nums, barX), Math.sub(argsLen, 1)))
+    return S
+
+
+def main():
+    # Fills up nums[] array with values
+    nums = read_stdin()
+
+    # Calcualates the standard_deviation using the numbers from the nums array
+    S = standard_deviation(nums)
+    print(S)
+
+if __name__ == "__main__":
+    cProfile.run('main()', "profiling_stats")
+    p = pstats.Stats("profiling_stats")
+    p.strip_dirs().sort_stats(SortKey.TIME).print_stats()
+
+
--- src/profiling_tests/generate_nums.py
+++ src/profiling_tests/generate_nums.py
@@ -4,14 +4,14 @@ def generate_floats(num_floats, lower_bound, upper_bound):
     return [random.uniform(lower_bound, upper_bound) for _ in range(num_floats)]
 
 # Number of floats to generate
-num_floats = 1_000
+num_floats = 100_000_000
 # Interval bounds
-lower_bound = -1_000_000
-upper_bound = 1_000_000
+lower_bound = -1_000_000_000
+upper_bound = 1_000_000_000
 
 if __name__ == "__main__":
     floats = generate_floats(num_floats, lower_bound, upper_bound)
     # Num of decimals_places = 7
-    decimals_places = 7
+    decimals_places = 10
     rounded_floats = [round(fl, decimals_places) for fl in floats]
     print(" ".join(map(str, rounded_floats)))
