d0f784ee178d1bda03c9244fc7182c7345bc8d54
Pavel Hajek <xhajekp00@vutbr.cz>
2024-04-23 08:01:02 -0700

    Renamed files created by installer

--- src/script.sh
+++ src/script.sh
@@ -2,8 +2,8 @@ mkdir -p ../installer/usr/share/calc 2>/dev/null
 cp  calculatormathlib.py ../installer/usr/share/calc/calculatormathlib.py
 cp  calculator.py ../installer/usr/share/calc/calculator.py && chmod +x ../installer/usr/share/calc/calculator.py
 mkdir -p ../installer/usr/local/bin 2>/dev/null
-ln -sf /usr/share/calc/calculator.py ../installer/usr/local/bin/calc-test
+ln -sf /usr/share/calc/calculator.py ../installer/usr/local/bin/Calculator
 mkdir ../installer/tmp 2>/dev/null
 cp python_requirements.txt ../installer/tmp/python_requirements.txt
 chmod +x ../installer/DEBIAN/postinst
-dpkg-deb --build ../installer/ ../installer/installer-test.deb
+dpkg-deb --build ../installer/ ../installer/calc_installer.deb
