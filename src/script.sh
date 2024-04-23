83325f71ef7d64769031e005f54dc8cc5c499aa2
Pavel Hajek <xhajekp00@vutbr.cz>
2024-04-23 23:08:52 +0200

    Changed folder name inside script and deleted removing files

--- src/script.sh
+++ src/script.sh
@@ -1,13 +1,11 @@
-mkdir -p ../installer/usr/share/calc 2>/dev/null
-cp  calculatormathlib.py ../installer/usr/share/calc/calculatormathlib.py
-cp  calculator.py ../installer/usr/share/calc/calculator.py && chmod +x ../installer/usr/share/calc/calculator.py
+mkdir -p ../installer/usr/share/ivs_calc 2>/dev/null
+cp  calculatormathlib.py ../installer/usr/share/ivs_calc/calculatormathlib.py
+cp  calculator.py ../installer/usr/share/ivs_calc/calculator.py
+chmod +x ../installer/usr/share/ivs_calc/calculator.py
 mkdir -p ../installer/usr/local/bin 2>/dev/null
-ln -sf /usr/share/calc/calculator.py ../installer/usr/local/bin/Calculator
+ln -sf /usr/share/ivs_calc/calculator.py ../installer/usr/local/bin/Calculator
 mkdir ../installer/tmp 2>/dev/null
 cp python_requirements.txt ../installer/tmp/python_requirements.txt
 chmod +x ../installer/DEBIAN/postinst
-dpkg-deb --build ../installer/ ../installer/calc_installer.deb
 
-mv ../installer/calc_installer.deb ..
-rm -r ../installer/usr
-rm -r ../installer/tmp
+dpkg-deb --build ../installer/ ../calc_installer.deb
