019616477de3703f531ff5657f9c074903e849e7
GorgosekPC <nezbeda.adam@gmail.com>
2024-04-24 02:33:25 +0200

    Fixed problems with installer not working

--- src/script_calc.sh
+++ src/script_calc.sh
@@ -1,11 +1,11 @@
-mkdir -p ../installer/usr/share/ivs_calc 2>/dev/null
-cp  calculatormathlib.py ../installer/usr/share/ivs_calc/calculatormathlib.py
-cp  calculator.py ../installer/usr/share/ivs_calc/calculator.py
-chmod +x ../installer/usr/share/ivs_calc/calculator.py
+mkdir -p ../installer/usr/share/ivs-calc 2>/dev/null
+cp  calculatormathlib.py ../installer/usr/share/ivs-calc/calculatormathlib.py
+cp  calculator.py ../installer/usr/share/ivs-calc/calculator.py
+chmod +x ../installer/usr/share/ivs-calc/calculator.py
 mkdir -p ../installer/usr/local/bin 2>/dev/null
-ln -sf /usr/share/ivs_calc/calculator.py ../installer/usr/local/bin/Calculator
+ln -sf /usr/share/ivs-calc/calculator.py ../installer/usr/local/bin/ivs-calc
 mkdir ../installer/tmp 2>/dev/null
 cp python_requirements.txt ../installer/tmp/python_requirements.txt
 chmod +x ../installer/DEBIAN/postinst
 
-dpkg-deb --build ../installer/ ../calc_installer.deb
+dpkg-deb --build ../installer/ ../ivs-calc_installer.deb
